8d997f6375475ea94521cc38f5e5a5fc4e29fea8
speehive <speehive@DESKTOP-TPF0JOE>
2022-12-21 16:43:58 +0530

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleRentalWebsite.Models
+{
+    public class Car
+    {
+        public string Name { get; set; }
+
+        public int Model { get; set; }
+
+        public int Price { get; set; }
+
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleRentalWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
